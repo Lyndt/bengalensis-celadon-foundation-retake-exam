2c40bbcdfedc5d6c194b15633d6c2b1648853156
Linda <m4rn0ta@gmail.com>
2022-01-14 12:16:06 +0100

    Multiply matrix exercise done

--- retakeexam/src/com/MultiplyItemsInMatrix.java
+++ retakeexam/src/com/MultiplyItemsInMatrix.java
@@ -1,5 +1,7 @@
 package com;
 
+import java.util.Arrays;
+
 public class MultiplyItemsInMatrix {
     public static void main(String[] args) {
         int[][] matrix1 = {
@@ -14,9 +16,27 @@ public class MultiplyItemsInMatrix {
                 {3, 3, 1, 5},
                 {18, 0, 10, 6}
         };
+        System.out.println(Arrays.deepToString(multiplyInMatrix(matrix1)));
+        System.out.println(Arrays.deepToString(multiplyInMatrix(matrix2)));
+//        System.out.println(multiplyInMatrix(matrix1));
+//        System.out.println(multiplyInMatrix(matrix2));
     }
     public static boolean isEven(int number) {
         return number % 2 == 0;
+    }
+    public static int[][] multiplyInMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (isEven(i) && isEven(matrix[i][j])) {
+                    matrix[i][j] = 2 * matrix[i][j];
+                }
+
+                if (!isEven(i) && !isEven(matrix[i][j])) {
+                    matrix[i][j] = 2 * matrix[i][j];
+                }
+            }
+        }
+        return matrix;
     }
 
 }
