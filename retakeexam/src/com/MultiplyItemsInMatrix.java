5dd3f9cb41ed63e992584d4a1a170614952be15e
Linda <m4rn0ta@gmail.com>
2022-01-14 12:08:27 +0100

    main method with two matrix done

---
+++ retakeexam/src/com/MultiplyItemsInMatrix.java
@@ -0,0 +1,18 @@
+package com;
+
+public class MultiplyItemsInMatrix {
+    public static void main(String[] args) {
+        int[][] matrix1 = {
+                {1, 3, 6, 2},
+                {7, 5, 6, 1},
+                {3, 3, 1, 5},
+                {9, 0, 5, 3}
+        };
+        int[][] matrix2 = {
+                {1, 3, 12, 4},
+                {14, 10, 6, 2},
+                {3, 3, 1, 5},
+                {18, 0, 10, 6}
+        };
+    }
+}
