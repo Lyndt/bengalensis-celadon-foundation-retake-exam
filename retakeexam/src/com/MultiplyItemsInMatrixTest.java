4738c15329def6c737f968d83fbdac7e333372dc
Linda <m4rn0ta@gmail.com>
2022-01-14 12:33:16 +0100

    Multiply matrix test not done yet

---
+++ retakeexam/src/com/MultiplyItemsInMatrixTest.java
@@ -0,0 +1,14 @@
+package com;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MultiplyItemsInMatrixTest {
+
+    @org.junit.jupiter.api.Test
+    void isEven() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void multiplyInMatrix() {
+    }
+}
