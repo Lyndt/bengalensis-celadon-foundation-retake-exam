509d5b1814cc057136c4a2e301eb7adce021d537
Linda <m4rn0ta@gmail.com>
2022-01-14 12:43:05 +0100

    Multiply matrix exercise first test done

--- retakeexam/src/com/MultiplyItemsInMatrixTest.java
+++ retakeexam/src/com/MultiplyItemsInMatrixTest.java
@@ -1,14 +1,27 @@
 package com;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MultiplyItemsInMatrixTest {
-
-    @org.junit.jupiter.api.Test
-    void isEven() {
-    }
-
-    @org.junit.jupiter.api.Test
+    @Test
     void multiplyInMatrix() {
+        int[][] input = {
+                {1, 3, 6, 2},
+                {7, 5, 6, 1},
+                {3, 3, 1, 5},
+                {9, 0, 5, 3}
+        };
+
+        int[][] expected = {
+                {1, 3, 12, 4},
+                {14, 10, 6, 2},
+                {3, 3, 1, 5},
+                {18, 0, 10, 6}
+        };
+        assertArrayEquals(expected, input);
     }
+
 }
+
